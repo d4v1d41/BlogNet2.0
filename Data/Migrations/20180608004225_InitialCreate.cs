a201b90816284107514580b561c39fb257d48e89
David Ramos Penott <davepenott@protonmail.com>
2018-06-07 22:07:11 -0300

    First commit

---
+++ Data/Migrations/20180608004225_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogNet.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blog",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    Time = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(maxLength: 500, nullable: true),
+                    UrlPic = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blog", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blog");
+        }
+    }
+}
---
+++ Models/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+
+namespace BlogNet.Models
+{
+    public class Blog
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [DisplayName(" Year/s of Experience")]
+        public string Time { get; set; }
+        [StringLength(500, MinimumLength = 60)]
+        public string Description { get; set; }
+        [DisplayName("URL of the company’s logo")]
+        public string UrlPic { get; set; }
+    }
+}
